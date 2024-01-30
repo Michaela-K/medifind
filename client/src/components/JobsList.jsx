a966cb5c5d2854acd19421a021bd4c4604a70765
Michaela-K <michaelakodes@gmail.com>
2024-01-30 15:04:01 -0500

    sset

--- client/src/components/JobsList.jsx
+++ client/src/components/JobsList.jsx
@@ -3,10 +3,10 @@ import { JobsContext } from "../context/index";
 import Modal from "./Modal";
 import { FaCheckToSlot } from "react-icons/fa6";
 
-import ReactTimeAgo from 'react-time-ago'
-import TimeAgo from 'javascript-time-ago'
-import en from 'javascript-time-ago/locale/en.json'
-TimeAgo.addDefaultLocale(en)
+import ReactTimeAgo from "react-time-ago";
+import TimeAgo from "javascript-time-ago";
+import en from "javascript-time-ago/locale/en.json";
+TimeAgo.addDefaultLocale(en);
 
 const JobsList = () => {
   const { jobData, setJobData } = useContext(JobsContext);
@@ -16,8 +16,8 @@ const JobsList = () => {
   const [jobStatus, setJobStatus] = useState({ is_filled: false });
 
   const clickedJob = (jobId) => {
-  return jobData.find((job) => job.id === jobId);
-};
+    return jobData.find((job) => job.id === jobId);
+  };
 
   const handleupdateJob = async (e) => {
     e.preventDefault();
@@ -36,10 +36,10 @@ const JobsList = () => {
 
       if (response.ok) {
         setJobData((prevJobData) =>
-        prevJobData.map((job) =>
-          job.id === id ? { ...job, is_filled: jobStatus.is_filled } : job
-        )
-      );
+          prevJobData.map((job) =>
+            job.id === id ? { ...job, is_filled: jobStatus.is_filled } : job
+          )
+        );
         console.log("Job Status updated successfully");
       } else {
         // Handle error cases here
@@ -51,9 +51,15 @@ const JobsList = () => {
     window.location.reload();
   };
 
-  useEffect(() => {
-   
-  }, [jobData]);
+  const cancelShift = (shift) => {
+    
+  };
+
+  const handleCancel = (shift) => {
+    cancelShift(shift);
+  };
+
+  useEffect(() => {}, [jobData]);
 
   return (
     <div className="flex flex-col h-[100vh] m-20 min-w-[320px]">
@@ -180,14 +186,18 @@ const JobsList = () => {
             <div
               key={index}
               className="relative overflow-hidden h-[300px] max-w-[400px] rounded-lg bg-white text-left shadow-[0_2px_15px_-3px_rgba(0,0,0,0.07),0_10px_20px_-2px_rgba(0,0,0,0.04)] "
-            > 
-            {/* {job.id} */}
+            >
+              {/* {job.id} */}
               <div className="pt-1 px-6 z-0">
                 <div className="flex justify-between pt-5">
                   <h5 className="mb-2.5 text-xl font-medium leading-tight text-neutral-800">
                     {job.facility_name}
                   </h5>
-                  {job.is_filled ? (<p className="flex justify-end text-green-500 text-4xl"><FaCheckToSlot /></p>) : null}
+                  {job.is_filled ? (
+                    <p className="flex justify-end text-green-500 text-4xl">
+                      <FaCheckToSlot />
+                    </p>
+                  ) : null}
                 </div>
                 <p className="mb-4 text-base">{job.title}</p>
               </div>
@@ -213,7 +223,11 @@ const JobsList = () => {
                 </div>
                 <div className="px-1 py-3">
                   {" "}
-                  <ReactTimeAgo key={index} date={Date.parse(job.date)} locale="en-US"/>
+                  <ReactTimeAgo
+                    key={index}
+                    date={Date.parse(job.date)}
+                    locale="en-US"
+                  />
                 </div>
               </div>
             </div>
@@ -221,60 +235,73 @@ const JobsList = () => {
       </div>
       {open ? (
         <>
-        <Modal>
-          <div className="relative flex flex-col gap-2 bg-white p-8 rounded-lg h-[50vh] w-[30vw]">
-            <h1 className="text-5xl text-black mt-4 mb-1">{clickedJob(jobId).title}</h1>
-            <hr />
-            <h1 className="text-3xl text-black mt-1">{clickedJob(jobId).facility_name}</h1>
-            <div className="flex w-full justify-between text-2xl font-medium">
-              <p className="text-1xl text-black mt-1">{clickedJob(jobId).type_of_worker}</p>
-              <p className="text-1xl text-black mt-1">${clickedJob(jobId).rate}/ hr</p>
+          <Modal>
+            <div className="relative flex flex-col gap-2 bg-white p-8 rounded-lg h-[50vh] w-[30vw]">
+              <h1 className="text-5xl text-black mt-4 mb-1">
+                {clickedJob(jobId).title}
+              </h1>
+              <hr />
+              <h1 className="text-3xl text-black mt-1">
+                {clickedJob(jobId).facility_name}
+              </h1>
+              <div className="flex w-full justify-between text-2xl font-medium">
+                <p className="text-1xl text-black mt-1">
+                  {clickedJob(jobId).type_of_worker}
+                </p>
+                <p className="text-1xl text-black mt-1">
+                  ${clickedJob(jobId).rate}/ hr
+                </p>
+              </div>
+              <p className="text-[1.285rem] text-black mt-1">
+                Gender required: {clickedJob(jobId).gender}
+              </p>
+              <p className="text-black mt-1">
+                It has survived not only five centuries, but also the leap into
+                electronic typesetting, remaining essentially unchanged. It was
+                popularised in the 1960s with the release of Letraset sheets
+                containing Lorem Ipsum passages, and more recently with desktop
+                publishing software like Aldus PageMaker including versions of
+                Lorem Ipsum.
+              </p>
+              {clickedJob(jobId).is_filled ? (
+                <div className="absolute w-full pr-16 py-8 bottom-0 flex flex-row gap-2">
+                  <button
+                    onClick={() => setOpen(!open)}
+                    className="flex-1 py-2 px-4 bg-gray-200 hover:bg-gray-300 text-gray-600 font-bold text-lg rounded-lg"
+                  >
+                    Close
+                  </button>
+                  <button
+                    onClick={() => cancelShift(clickedJob)}
+                    className="flex-1 bg-[#9e2e2a] text-white inline-block rounded-lg px-6 pb-2 pt-2.5 font-medium uppercase shadow-[0_2px_7px_-4px_#3b71ca] transition duration-150 ease-in-out hover:bg-primary-600 hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:bg-primary-600 focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:outline-none focus:ring-0 active:bg-primary-700"
+                  >
+                    Cancel My Shift
+                  </button>
+                </div>
+              ) : (
+                <div className="absolute w-full pr-16 py-8 bottom-0 flex flex-row gap-2">
+                  <button
+                    onClick={() => setOpen(!open)}
+                    className="flex-1 py-2 px-4 bg-gray-200 hover:bg-gray-300 text-gray-600 font-bold text-lg rounded-lg"
+                  >
+                    Cancel
+                  </button>
+                  <button
+                    onClick={(e) => {
+                      handleupdateJob(e);
+                      setJobStatus((prevJobStatus) => ({
+                        ...prevJobStatus,
+                        is_filled: !prevJobStatus.is_filled,
+                      }));
+                    }}
+                    className="flex-1 bg-[#7B84D3] text-white inline-block rounded-lg px-6 pb-2 pt-2.5 font-medium uppercase shadow-[0_2px_7px_-4px_#3b71ca] transition duration-150 ease-in-out hover:bg-primary-600 hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:bg-primary-600 focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:outline-none focus:ring-0 active:bg-primary-700"
+                  >
+                    Accept Job
+                  </button>
+                </div>
+              )}
             </div>
-            <p className="text-[1.285rem] text-black mt-1">Gender required: {clickedJob(jobId).gender}</p>
-            <p className="text-black mt-1">
-              It has survived not only five centuries, but also the leap into
-              electronic typesetting, remaining essentially unchanged. It was
-              popularised in the 1960s with the release of Letraset sheets
-              containing Lorem Ipsum passages, and more recently with desktop
-              publishing software like Aldus PageMaker including versions of
-              Lorem Ipsum.
-            </p>
-            {clickedJob(jobId).is_filled  ? (
-              <div className="absolute w-full pr-16 py-8 bottom-0 flex flex-row gap-2">
-                 <button
-                  onClick={() => setOpen(!open)}
-                  className="flex-1 py-2 px-4 bg-gray-200 hover:bg-gray-300 text-gray-600 font-bold text-lg rounded-lg"
-                >
-                  Close
-                </button>
-                <button className="flex-1 bg-[#9e2e2a] text-white inline-block rounded-lg px-6 pb-2 pt-2.5 font-medium uppercase shadow-[0_2px_7px_-4px_#3b71ca] transition duration-150 ease-in-out hover:bg-primary-600 hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:bg-primary-600 focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:outline-none focus:ring-0 active:bg-primary-700">
-                  Cancel This Shift
-                </button>
-              </div>
-            ) : (
-              <div className="absolute w-full pr-16 py-8 bottom-0 flex flex-row gap-2">
-                <button
-                  onClick={() => setOpen(!open)}
-                  className="flex-1 py-2 px-4 bg-gray-200 hover:bg-gray-300 text-gray-600 font-bold text-lg rounded-lg"
-                >
-                  Cancel
-                </button>
-                <button
-                  onClick={(e) => {
-                    handleupdateJob(e);
-                    setJobStatus((prevJobStatus) => ({
-                      ...prevJobStatus,
-                      is_filled: !prevJobStatus.is_filled
-                    }));
-                  }}
-                  className="flex-1 bg-[#7B84D3] text-white inline-block rounded-lg px-6 pb-2 pt-2.5 font-medium uppercase shadow-[0_2px_7px_-4px_#3b71ca] transition duration-150 ease-in-out hover:bg-primary-600 hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:bg-primary-600 focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:outline-none focus:ring-0 active:bg-primary-700"
-                >
-                  Accept Job
-                </button>
-              </div>
-            )}
-          </div>
-        </Modal>
+          </Modal>
         </>
       ) : null}
     </div>
