cd2a36584d48a6892b7019bf347fbdcc972f1c6b
Michaela-K <michaelakodes@gmail.com>
2024-01-31 10:28:11 -0500

    update styling for new job page

--- client/src/components/NewJob.jsx
+++ client/src/components/NewJob.jsx
@@ -1,84 +1,127 @@
-import React from 'react'
+import React from "react";
 
 const NewJob = () => {
   return (
+    <div className="relative flex justify-center min-h-screen overflow-hidden">
+      <div className="w-[1000px] p-12 m-auto bg-white rounded-md shadow-md max-w-[80vw] min-w-[350px]">
+        <h1 className="text-5xl pb-8 text-[#24233E] text-center">
+          New Job
+        </h1>
+        <form className="mt-4">
+          <div className="mb-4">
+            <label
+              htmlFor="title"
+              className="block text-md text-gray-600"
+            >
+              Title
+            </label>
+            <input
+              type="text"
+              className="block w-full px-4 py-2 mt-2 bg-white border rounded-md focus:border-[#6c77d1] focus:ring-[#6c77d1] focus:outline-none focus:ring focus:ring-opacity-10"
+            />
+          </div>
+          <div className="mb-4">
+            <label
+              htmlFor="worker"
+              className="block text-md text-gray-600"
+            >
+              Type of Worker requested
+            </label>
+            <input
+              type="text"
+              className="block w-full px-4 py-2 mt-2 bg-white border rounded-md focus:border-[#6c77d1] focus:ring-[#6c77d1] focus:outline-none focus:ring focus:ring-opacity-10"
+            />
+          </div>
+          <div className="mb-4">
+            <label
+              htmlFor="rate"
+              className="block text-md text-gray-600"
+            >
+              Rate
+            </label>
+            <input
+              type="number"
+              className="block w-full px-4 py-2 mt-2 bg-white border rounded-md focus:border-[#6c77d1] focus:ring-[#6c77d1] focus:outline-none focus:ring focus:ring-opacity-10"
+            />
+          </div>
+          <div className="mb-4">
+            <label
+              htmlFor="gender"
+              className="block text-md text-gray-600"
+            >
+              Gender
+            </label>
+            <input
+              type="text"
+              className="block w-full px-4 py-2 mt-2 bg-white border rounded-md focus:border-[#6c77d1] focus:ring-[#6c77d1] focus:outline-none focus:ring focus:ring-opacity-10"
+            />
+          </div>
+          <div className="mb-4">
+            <label
+              htmlFor="date"
+              className="block text-md text-gray-600"
+            >
+              Date
+            </label>
+            <input
+              type="date"
+              className="block w-full px-4 py-2 mt-2 bg-white border rounded-md focus:border-[#6c77d1] focus:ring-[#6c77d1] focus:outline-none focus:ring focus:ring-opacity-10"
+            />
+          </div>
+          <div className="mb-4">
+            <label
+              htmlFor="start_time"
+              className="block text-md text-gray-600"
+            >
+              Start Time
+            </label>
+            <input
+              type="time"
+              className="block w-full px-4 py-2 mt-2 bg-white border rounded-md focus:border-[#6c77d1] focus:ring-[#6c77d1] focus:outline-none focus:ring focus:ring-opacity-10"
+            />
+          </div>
+          <div className="mb-4">
+            <label
+              htmlFor="facility_name"
+              className="block text-md text-gray-600"
+            >
+              Name Of Facility
+            </label>
+            <input
+              type="text"
+              className="block w-full px-4 py-2 mt-2 bg-white border rounded-md focus:border-[#6c77d1] focus:ring-[#6c77d1] focus:outline-none focus:ring focus:ring-opacity-10"
+            />
+          </div>
+          <div className="mb-4">
+            <label
+              htmlFor="facility_short_address"
+              className="block text-md text-gray-600"
+            >
+              Facility Address
+            </label>
+            <input
+              type="text"
+              className="block w-full px-4 py-2 mt-2 bg-white border rounded-md focus:border-[#6c77d1] focus:ring-[#6c77d1] focus:outline-none focus:ring focus:ring-opacity-10"
+            />
+          </div>
 
-        <div className="relative flex flex-col justify-center min-h-screen overflow-hidden">
-<div className="w-full p-6 m-auto bg-white rounded-md shadow-xl shadow-rose-600/40 ring-2 ring-indigo-600 lg:max-w-xl">
-    <h1 className="text-3xl font-semibold text-center text-indigo-700 underline uppercase decoration-wavy">
-       New Job
-    </h1>
-    <form className="mt-6">
-        <div className="mb-2">
-            <label
-                for="email"
-                className="block text-sm font-semibold text-gray-800"
-            >
-                Firstname
-            </label>
-            <input
-                type="email"
-                className="block w-full px-4 py-2 mt-2 text-indigo-700 bg-white border rounded-md focus:border-indigo-400 focus:ring-indigo-300 focus:outline-none focus:ring focus:ring-opacity-40"
-            />
-        </div>
-        <div className="mb-2">
-            <label
-                for="email"
-                className="block text-sm font-semibold text-gray-800"
-            >
-                Lastname
-            </label>
-            <input
-                type="email"
-                className="block w-full px-4 py-2 mt-2 text-indigo-700 bg-white border rounded-md focus:border-indigo-400 focus:ring-indigo-300 focus:outline-none focus:ring focus:ring-opacity-40"
-            />
-        </div>
-        <div className="mb-2">
-            <label
-                for="email"
-                className="block text-sm font-semibold text-gray-800"
-            >
-                Email
-            </label>
-            <input
-                type="email"
-                className="block w-full px-4 py-2 mt-2 text-indigo-700 bg-white border rounded-md focus:border-indigo-400 focus:ring-indigo-300 focus:outline-none focus:ring focus:ring-opacity-40"
-            />
-        </div>
-        <div className="mb-2">
-            <label
-                for="password"
-                className="block text-sm font-semibold text-gray-800"
-            >
-                Password
-            </label>
-            <input
-                type="password"
-                className="block w-full px-4 py-2 mt-2 text-indigo-700 bg-white border rounded-md focus:border-indigo-400 focus:ring-indigo-300 focus:outline-none focus:ring focus:ring-opacity-40"
-            />
-        </div>
-        <div className="mt-6">
-            <button className="w-full px-4 py-2 tracking-wide text-white transition-colors duration-200 transform bg-indigo-700 rounded-md hover:bg-indigo-600 focus:outline-none focus:bg-indigo-600">
-                Login
+          <div className="mt-6">
+            <button className="w-full px-4 py-2 tracking-wide text-white transition-colors duration-200 transform bg-indigo-700 rounded-md hover:bg-indigo-600">
+              Login
             </button>
-        </div>
-    </form>
+          </div>
+        </form>
 
-    <p className="mt-8 text-xs font-light text-center text-gray-700">
-        {" "}
-        Already have an account?{" "}
-        <a
-            href="#"
-            className="font-medium text-indigo-600 hover:underline"
-        >
+        <p className="mt-16 text-lg font-light text-center text-gray-700">
+          {" "}
+          Already have an account?{" "}
+          <a href="#" className="font-medium text-indigo-600 hover:underline">
             Sign in
-        </a>
-    </p>
-</div>
-</div>
+          </a>
+        </p>
+      </div>
+    </div>
+  );
+};
 
-  )
-}
-
-export default NewJob
-
+export default NewJob;
